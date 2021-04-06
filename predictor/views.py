01d7a1f8b8850fa47dcc130826f5bf803c0b8c41
Karina Kozarova <karinakozarova1303@gmail.com>
2021-04-06 23:45:03 +0200

    base app added

---
+++ predictor/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .apps import PredictorConfig
+from django.http import JsonResponse
+from rest_framework.views import APIView
+
+from django.http import HttpResponse
+
+class call_model(APIView):
+    def get(self,request):
+        if request.method == 'GET':
+            # get sound from request
+            tweet = request.GET.get('tweet')
+            vectorizeTweet = PredictorConfig.vectorizer.transform([tweet])
+            
+            prediction = PredictorConfig.regressor.predict(vectorizeTweet)[0]
+            
+            response = {'IsHateSpeech': bool(prediction)}
+            print(response)
+            
+            return JsonResponse(response)
---
+++ web/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def homePageView(request):
+    return render(request, 'classify.html')
